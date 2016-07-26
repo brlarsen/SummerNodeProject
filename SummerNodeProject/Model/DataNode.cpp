979ee501f2e3ccdaccfb2e40a2cc12105071ced5
brlarsen <cody.henrichsen@740s-pl-124.local>
2016-07-26 15:11:57 -0600

    Implemented all DataNode Classes

--- SummerNodeProject/Model/DataNode.cpp
+++ SummerNodeProject/Model/DataNode.cpp
@@ -7,3 +7,35 @@
 //
 
 #include "DataNode.hpp"
+
+
+//Constructors
+template <class Type>
+DataNode<Type> :: DataNode()
+{
+    this->nodeData = nullptr;
+}
+
+template <class Type>
+DataNode<Type> :: DataNode(Type nodeData)
+{
+    this->nodeData = nodeData;
+    this->nodePointer = nullptr;
+}
+// End constructors
+
+//Getters and Setters
+
+template <class Type>
+Type DataNode<Type> :: getNodeData()
+{
+    return nodeData;
+}
+
+template <class Type>
+void DataNode<Type> :: setNodePointer(DataNode<Type>* nodePointer)
+{
+    this->nodePointer = nodePointer;
+}
+
+//End Getters/Setters
