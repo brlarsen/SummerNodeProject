08d00e99071b85ba060c56d3e66fd68eeec3f592
brlarsen <cody.henrichsen@740s-pl-124.local>
2016-07-27 14:46:38 -0600

    Made a method.

--- SummerNodeProject/Model/SummerList.cpp
+++ SummerNodeProject/Model/SummerList.cpp
@@ -23,18 +23,48 @@ void SummerList<Type> :: addAtIndex(int index, Type data)
     assert(index >= 0 && index <= size);
     //Different from array - we can add to the end.
     DataNode<Type> *indexPointer = front;
-    
-    //Get to the spot before the spot I am adding to.
-    for (int position = 0; position < index - 1; position++)
-    {
-        indexPointer = indexPointer -> getNodePointer();
-    }
-    
+
     //Create a node.
     DataNode<Type> * newNode = new DataNode<Type>(data);
     
-    newNode -> setNodePointer(indexPointer -> getNodePointer);
+if(index == 0)
+{
+    newNode -> setNodePointer(indexPointer);
+    front = newNode;
+}
+else if(index == size - 1)
+    {
+        add(data);
+    }
+    else
+    {
+        //Get to the spot before the spot I am adding to.
+        for (int position = 0; position < index - 1; position++)
+        {
+            indexPointer = indexPointer -> getNodePointer();
+        }
     
+    //Pointed newNode to the address of indexPointers next node
     DataNode<Type> * temp = indexPointer -> getNodePointer();
     newNode -> setNodePointer(temp);
+    
+    //Setting indexPointer to point the newNodes address
+    indexPointer -> setNodePointer(newNode);
+    }
+}
+
+template <class Type>
+void SummerList<Type> :: add(Type data)
+{
+    DataNode<Type> * newNode = new DataNode<Type>(data);
+    if(size == 0)
+    {
+        front = newNode;
+        end = newNode;
+    }
+    else
+    {
+        end -> setNodePointer(newNode);
+        end = newNode;
+    } 
 }
