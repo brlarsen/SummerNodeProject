8a21663f4caeebfae76e3a15444f8cdd45c0c9fc
brlarsen <cody.henrichsen@740s-pl-124.local>
2016-07-28 10:31:00 -0600

    Built Binary Tree

---
+++ SummerNodeProject/Model/BinaryTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTree.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/28/16.
+//  Copyright © 2016 workshop. All rights reserved.
+//
+
+#include "BinaryTree.hpp"
--- SummerNodeProject/Model/BinaryTreeNode.cpp
+++ SummerNodeProject/Model/BinaryTreeNode.cpp
@@ -7,3 +7,68 @@
 //
 
 #include "BinaryTreeNode.hpp"
+
+
+//Constructors
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : DataNode<Type>()
+{
+    parent = nullptr;
+    leftChild = nullptr;
+    rightChild = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : DataNode<Type>(data)
+{
+    //this -> setNodeData(data); This is not needed because of DataNode<Type>(data) above
+    parent = nullptr;
+    leftChild = nullptr;
+    rightChild = nullptr;
+    
+}
+
+//Getters and Setters
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeftChild()
+{
+    return leftChild;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightChild()
+{
+    return rightChild;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getparent()
+{
+    return parent;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setLeftChild(BinaryTreeNode<Type> * leftChild)
+{
+    this -> leftChild = leftChild;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRightChild(BinaryTreeNode<Type> * rightChild)
+{
+    this -> rightChild = rightChild;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setParent(BinaryTreeNode<Type> * parent)
+{
+    this -> parent = parent;
+}
+
+
+
+//Java > C++
+//&&
+//Mac > PC
